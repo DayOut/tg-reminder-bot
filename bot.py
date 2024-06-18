8ec40f9beb47d6189ff5fa12eab9459f4f1f6e0b
Dayout <fearess7@gmail.com>
2024-06-18 12:23:50 +0300

    Show current time

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import asyncio
+from datetime import datetime
 from telegram import Bot
 import schedule
 import os
@@ -8,16 +9,16 @@ import os
 # Налаштування логування
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelень)-%(message)s', level=logging.INFO)
 
-
 # Отримання змінних оточення
 bot_token = os.environ.get('TELEGRAM_BOT_TOKEN')
 chat_id = os.environ.get('TELEGRAM_CHAT_ID')
 events_json = os.environ.get('EVENTS')
 
+
 # Асинхронна функція для відправлення повідомлення
-async def send_message(message):
-    await bot.send_message(chat_id=chat_id, text=message, disable_notification=True)
-    logging.info(f"Sent message: {message}")
+async def send_message(message, disable_notification=False):
+    await bot.send_message(chat_id=chat_id, text=message, disable_notification=disable_notification)
+    logging.info(f"Sent message: {message} with disable_notification={disable_notification}")
 
 
 # Налаштування розкладу нагадувань
@@ -34,8 +35,11 @@ async def scheduler():
 
 # Основна функція
 async def main():
-    # Відправлення повідомлення при старті програми
-    await send_message("Програма запущена! Нагадування активні.")
+    # Отримання поточної дати та часу
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # Відправлення повідомлення при старті програми без сповіщення
+    await send_message(f"Програма запущена! Нагадування активні. Поточний час: {current_time}",
+                       disable_notification=True)
 
     schedule_tasks()
     await scheduler()
@@ -43,6 +47,5 @@ async def main():
 
 if __name__ == '__main__':
     events = json.loads(events_json)
-
     bot = Bot(token=bot_token)
     asyncio.run(main())
