78eec12177be19daf171b0d468d1fcaf1cc21e26
Dayout <fearess7@gmail.com>
2024-06-18 12:29:21 +0300

    Update task time to real time

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
 import json
 import logging
 import asyncio
-from datetime import datetime
+from datetime import datetime, time
 from telegram import Bot
 import schedule
 import os
+from pytz import timezone, utc
 
 # Налаштування логування
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelень)-%(message)s', level=logging.INFO)
@@ -13,6 +14,18 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelень)-%(message)s'
 bot_token = os.environ.get('TELEGRAM_BOT_TOKEN')
 chat_id = os.environ.get('TELEGRAM_CHAT_ID')
 events_json = os.environ.get('EVENTS')
+local_timezone = os.environ.get('LOCAL_TIMEZONE', 'Europe/Kiev')  # Задання часового поясу за замовчуванням
+
+# Завантаження та парсинг подій
+events = json.loads(events_json)
+bot = Bot(token=bot_token)
+
+
+# Конвертація часу подій у локальний часовий пояс
+def convert_time_to_local(event_time, tz):
+    event_time_utc = datetime.strptime(event_time, "%H:%M").time()
+    event_time_local = datetime.combine(datetime.today(), event_time_utc).replace(tzinfo=utc).astimezone(tz)
+    return event_time_local.strftime("%H:%M")
 
 
 # Асинхронна функція для відправлення повідомлення
@@ -23,8 +36,10 @@ async def send_message(message, disable_notification=False):
 
 # Налаштування розкладу нагадувань
 def schedule_tasks():
+    tz = timezone(local_timezone)
     for event in events:
-        schedule.every().day.at(event['time']).do(asyncio.create_task, send_message(event['message']))
+        local_time = convert_time_to_local(event['time'], tz)
+        schedule.every().day.at(local_time).do(asyncio.create_task, send_message(event['message']))
 
 
 async def scheduler():
@@ -35,8 +50,10 @@ async def scheduler():
 
 # Основна функція
 async def main():
-    # Отримання поточної дати та часу
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # Отримання поточної дати та часу з врахуванням часового поясу
+    tz = timezone(local_timezone)
+    current_time = datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
+
     # Відправлення повідомлення при старті програми без сповіщення
     await send_message(f"Програма запущена! Нагадування активні. Поточний час: {current_time}",
                        disable_notification=True)
@@ -46,6 +63,4 @@ async def main():
 
 
 if __name__ == '__main__':
-    events = json.loads(events_json)
-    bot = Bot(token=bot_token)
     asyncio.run(main())
