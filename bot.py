70b630ac66c9c4fd7d4bf07abbf7d5c3e00e0342
Dayout <fearess7@gmail.com>
2024-06-18 12:08:50 +0300

    Added message when starting

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ import os
 # Налаштування логування
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelень)-%(message)s', level=logging.INFO)
 
+
 # Отримання змінних оточення
 bot_token = os.environ.get('TELEGRAM_BOT_TOKEN')
 chat_id = os.environ.get('TELEGRAM_CHAT_ID')
@@ -18,20 +19,27 @@ async def send_message(message):
     await bot.send_message(chat_id=chat_id, text=message)
     logging.info(f"Sent message: {message}")
 
+
 # Налаштування розкладу нагадувань
 def schedule_tasks():
     for event in events:
         schedule.every().day.at(event['time']).do(asyncio.create_task, send_message(event['message']))
+
 
 async def scheduler():
     while True:
         schedule.run_pending()
         await asyncio.sleep(1)
 
+
 # Основна функція
 async def main():
+    # Відправлення повідомлення при старті програми
+    await send_message("Програма запущена! Нагадування активні.")
+
     schedule_tasks()
     await scheduler()
+
 
 if __name__ == '__main__':
     events = json.loads(events_json)
