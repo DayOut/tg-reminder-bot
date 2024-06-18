e47d2a5fbd413d19359f4baff4b176b030c5dc37
Dayout <fearess7@gmail.com>
2024-06-18 16:18:51 +0300

    Add daily report

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s -%(message)s', level=logging.
 bot_token = os.environ.get('TELEGRAM_BOT_TOKEN', '7282102903:AAF6Wp4H-kuAho8oPZblM9_PD9W7XVx6EkA')
 chat_id = os.environ.get('TELEGRAM_CHAT_ID', '294086745')
 events_json = os.environ.get('EVENTS', '[{"message": "Випити Animal Flex)", "timeUTC": "12:00"},{"message": "TEST", "timeUTC": "11:30"}]')
-daily_report = os.environ.get('DAILY_REPORT', '12:52')
+daily_report = os.environ.get('DAILY_REPORT', '13:22')
 time_delta = os.environ.get('TIME_DELTA', '3')
 # local_timezone = os.environ.get('LOCAL_TIMEZONE', 'Europe/Kiev')  # Задання часового поясу за замовчуванням
 
@@ -53,9 +53,9 @@ def get_scheduled_tasks() -> str:
     hours, minutes = daily_report.split(':')
     message += f'>`time: {int(hours) + int(time_delta)}:{minutes} \(UTC: {daily_report}\) \- Daily report`\n'
 
-    message += '> joblist:\n'
-    for job in schedule.get_jobs():
-        message += f'> {str(job.next_run.strftime('%H:%M:%S'))}\n'
+    # message += '> joblist:\n'
+    # for job in schedule.get_jobs():
+    #     message += f'> {str(job.next_run.strftime('%H:%M:%S'))}\n'
     return message
 
 async def scheduler():
