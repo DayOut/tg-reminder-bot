9042ee242ae60c8b5dd89e04a842362116c41eff
Dayout <fearess7@gmail.com>
2024-06-18 13:31:03 +0300

    debugging

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s -%(message)s', level=logging.
 bot_token = os.environ.get('TELEGRAM_BOT_TOKEN', '7282102903:AAF6Wp4H-kuAho8oPZblM9_PD9W7XVx6EkA')
 chat_id = os.environ.get('TELEGRAM_CHAT_ID', '294086745')
 events_json = os.environ.get('EVENTS', '[{"message": "Випити Animal Flex)", "timeUTC": "12:00"},{"message": "TEST", "timeUTC": "11:30"}]')
-local_timezone = os.environ.get('LOCAL_TIMEZONE', 'Europe/Kiev')  # Задання часового поясу за замовчуванням
+# local_timezone = os.environ.get('LOCAL_TIMEZONE', 'Europe/Kiev')  # Задання часового поясу за замовчуванням
 
 # Завантаження та парсинг подій
 events = json.loads(events_json)
@@ -47,8 +47,8 @@ async def scheduler():
 # Основна функція
 async def main():
     # Отримання поточної дати та часу з врахуванням часового поясу
-    tz = timezone(local_timezone)
-    current_time = datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
+    # tz = timezone(local_timezone)
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     # Відправлення повідомлення при старті програми без сповіщення
     await send_message(f"Програма запущена! Нагадування активні. Поточний час: {current_time}",
